e234de320fe7408c0b660a1368ac63f8e844bf3b
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2025-12-27 13:28:01 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all Js code as newer Version
+ //alert(3+3) // We are using nodejs not browser
+
+ console.log(3
+    +
+    3)  // code readability should high
+ console.log("Satyam")
+
+ let name ="satyam" // String datatype
+ let age=18 //number datatype
+ let isLoggedin=false //boolean Datatype
+ let state=null
+
+//Datatype=>
+ //number->2 to power 53
+ //bigint
+ //String-it can be in Single coat aur double coat but prefer double ""
+ //boolean=>true/false
+ //null=>Standalone Value--It is a representation of empty value...we can assign null also
+ //undefined
+ //Symbol => unique
+
+
+ //Object
+ console.log(typeof "Satyam");
+ console.log(typeof null);  // null is a type of object
+ console.log(typeof undefined);
+

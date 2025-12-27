134c821a4b6e39a24f132ab6ef4fd241827c3aed
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2025-12-27 14:11:47 +0000

    Conversion Of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+//let score="33abc";value...>Nan
+//let score=null;//value..>0
+//let score=undefined;//value..>NaN
+let score=true //value-->1
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1,//false=>0
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+//1=>true 0=>false
+//""=>false
+//"Satyam=>true"
+
+let someNumber=44
+let stringOfSomeNumber=String(someNumber)
+console.log(typeof stringOfSomeNumber);
+console.log(stringOfSomeNumber)

3e0871d41fbcc169d41dd222dd1f4ae51c1f2240
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2025-12-28 13:30:30 +0000

    operation complete

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -37,18 +37,24 @@ let negvalue=-value
 // console.log(2**2);
 // console.log(2%2);
 let str1="hello"
-let str2=" Satyam"
-let str3=str1+str2
-console.log(str3)
+// let str2=" Satyam"
+// let str3=str1+str2
+// console.log(str3)
 
-console.log("1"+2)
-console.log("1"+2+2)
-console.log(1+"2")
-console.log(1+2+"2")
-console.log((3+4)*5%3)
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log((3+4)*5%3)
  
-console.log(true) //return true
-console.log(+true) //return 1
-console.log(+"") //return 0
+// console.log(true) //return true
+// console.log(+true) //return 1
+// console.log(+"") //return 0
 
 
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)

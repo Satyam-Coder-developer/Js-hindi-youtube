61e36c29d7fa381e9280c6a60974f5728f8070c9
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2025-12-30 14:15:21 +0000

    datatype-summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -24,3 +24,23 @@ const myFunction=function(){
     console.log("Hello World");
 }
 console.log(typeof bigNumber);
+
+
+//++++++++++++++++++++++++++++Memory++++++++++++++++++++++++++
+//There are two types of memory-Stack,heap
+//In Primitive type Stack memory is used and heap is used for non-prmitive type
+
+let myOriginalName="Satyam";
+let anotherName=myOriginalName
+anotherName="Satya"
+console.log(myOriginalName)
+console.log(anotherName)
+
+let userone={
+    email:"user@email.com",
+    upi:"user@ybl",
+}
+let userTwo=userone;
+userTwo.email="satyam@google.com"
+console.log(userone.email);
+console.log(userTwo.email);

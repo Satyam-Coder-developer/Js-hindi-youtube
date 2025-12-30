f0804b47bdf920a4f24879659cc8ca53f0a1c4b7
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2025-12-30 14:36:25 +0000

    String

---
+++ 01_basics/String.js
@@ -0,0 +1,7 @@
+const name="Satyam"
+const repoCount=50
+console.log(name+repoCount+" Value");
+console.log(`Hello my name is ${name} and my repoCont is ${repoCount}`);//Modern way of Syntax that is String interpolation
+
+const gameName=new String('satyamkumar')//new way to represent string
+console.log(gameName[0]);

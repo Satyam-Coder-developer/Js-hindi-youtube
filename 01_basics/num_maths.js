c377ef4749cea1d1aeadc32913440903ff70f717
Satyam Kumar Roy <satyamkumar3029@gmail.com>
2026-01-09 14:28:51 +0000

    Math

---
+++ 01_basics/num_maths.js
@@ -0,0 +1,35 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber=123.89
+console.log(otherNumber.toPrecision(3));
+
+const hundereds=100000;
+console.log(hundereds.toLocaleString('en-IN'));//American to Indian 1,00,000
+
+
+//+++++++++++++++++++Math+++++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(4)); // return absolute value:4
+console.log(Math.round(4.6)); // return round off value:5
+console.log(Math.ceil(4.2));//return upper value:5
+console.log(Math.floor(4.2));//return lower value:4
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+
+
+
+
+
+
+
